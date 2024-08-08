4253a7afe231b12bd700cb908c0ba350c070c0d6
aluno <aluno@UNIFEOB-ACAD.LOCAL>
2024-08-07 22:09:45 -0300

    feat: add cors

--- app/server/server.js
+++ app/server/server.js
@@ -1,12 +1,15 @@
 require("module-alias/register");
 const express = require("express");
 const env = require("./config/env.js");
+const cors = require("cors")
+
 const { router } = require("./router.js");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors())
 app.use(router);
 
 app.listen(env.port, () => {
